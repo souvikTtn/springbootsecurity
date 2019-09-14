1e71be880da2ff759ea4f3e6cd1dceb77495af5f
sdhyani <sdhyani@motus.com>
2019-09-14 14:54:07 +0530

    in memory authentication

---
+++ springSecurityPractice/src/main/java/com/example/springSecurity/springSecurityPractice/configuration/MySecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.springSecurity.springSecurityPractice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class MySecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+    throws Exception {
+        //set your configuration in the auth object
+        //configuring in memory authentication
+        auth.inMemoryAuthentication()
+            .withUser("souvik")
+            .password("ABCdef123@")
+            .roles("USER")
+            .and()
+            .withUser("subham")
+            .password("1234")
+            .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
